cabb8e42f4b819d146b733f1399d4beafa114006
Nicholas Macedo <33987691+nicholasmacedoo@users.noreply.github.com>
2024-04-30 19:08:14 -0300

    feat: Aula 3

--- aula2-lib-externas/src/App.jsx
+++ aula2-lib-externas/src/App.jsx
@@ -1,7 +1,8 @@
-import { Home } from './pages/home'
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './routes'
 
 function App() {
-  return <Home />
+  return <RouterProvider router={routes} />
 }
 
 export default App
